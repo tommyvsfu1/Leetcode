b8626e4ce5858ce4d96782fdd93ea01f49523f3e
vsfu1 <tommy558788@gmail.com>
2025-09-20 06:50:34 +0800

    Create Code.cpp

---
+++ Dynamic_Programming/516/Code.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int N = s.size();
+        s.insert(s.begin(), '#');
+
+        vector<vector<int>> dp(N+1, vector<int>(N+1, 0));
+        
+        for (int i = 1; i <= N; i++)
+            dp[i][i] = 1;
+
+        for (int len = 2; len <= N; len++) {
+            for (int i = 1; i <= N - len + 1; i++) {
+                int j = i + len - 1;
+
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }
+                else {
+                    dp[i][j] = max(dp[i][j-1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[1][N];
+    }
+};
+
+
