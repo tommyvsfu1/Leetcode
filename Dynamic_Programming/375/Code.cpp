deb5f1e75874d439550a4bdf37b66c46c962cfa3
vsfu1 <tommy558788@gmail.com>
2025-09-22 00:58:10 +0800

    Create Code.cpp

---
+++ Dynamic_Programming/375/Code.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+
+
+        for (int len = 1; len <= n; len++) {
+            for (int i = 1; i + len - 1 <= n; i++) {
+                int j = i + len - 1;
+
+                if (len == 1) continue; // dp[i][i] = 0;
+
+                dp[i][j] = INT_MAX/2;
+                for (int k = i; k <= j; k++) {
+                    dp[i][j] = min(dp[i][j], k + max(dp[i][k-1],dp[k+1][j]));
+                }
+            }
+        }
+        return dp[1][n];
+    }
+};
+
