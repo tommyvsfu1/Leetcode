b233f246ece5521db90041cf19f9db8846ef87cd
vsfu1 <tommy558788@gmail.com>
2025-10-28 01:58:15 +0800

    Update Code.cpp

--- Difference_Array/2132/Code.cpp
+++ Difference_Array/2132/Code.cpp
@@ -4,23 +4,23 @@ public:
     Prefix2D(vector<vector<int>>& A) {
         int m = A.size();
         int n = A[0].size();
-        presum.resize(m+1);
-        for (int i = 0; i <= m; i++)
-            presum[i].resize(n+1);
+        presum.resize(m);
+        for (int i = 0; i < m; i++)
+            presum[i].resize(n);
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int a = (i - 1 > 0) ?  presum[i-1][j] : 0;
-                int b = (j-1 > 0) ? presum[i][j-1] : 0;
-                int c = (i-1 > 0 && j-1 > 0) ? presum[i-1][j-1] : 0;
+                int a = (i > 0) ?  presum[i-1][j] : 0;
+                int b = (j > 0) ? presum[i][j-1] : 0;
+                int c = (i > 0 && j > 0) ? presum[i-1][j-1] : 0;
                 presum[i][j] = A[i][j] + a + b - c;
             }
         }
     }
     int query(int i, int j, int x, int y) {
-        int a = (i-1 > 0 && j-1 > 0) ? presum[i-1][j-1] : 0;
-        int b = (j-1 > 0) ? presum[x][j-1] : 0;
-        int c = (i-1 > 0) ? presum[i-1][y] : 0;
+        int a = (i > 0 && j > 0) ? presum[i-1][j-1] : 0;
+        int b = (j > 0) ? presum[x][j-1] : 0;
+        int c = (i > 0) ? presum[i-1][y] : 0;
         return presum[x][y] - b - c + a;
     }
 };
@@ -29,16 +29,16 @@ public:
     bool possibleToStamp(vector<vector<int>>& grid, int stampHeight, int stampWidth) {
         
         Prefix2D Grid(grid);
-        vector<vector<int>> stamps;
         int m = grid.size();
         int n = grid[0].size();
+        vector<vector<int>> stamps(m, vector<int>(n,0));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int x = i + stampHeight - 1;
                 int y = j + stampWidth - 1;
                 if (x >= m || y >= n) continue;
                 if (Grid.query(i, j, x, y) == 0)
-                    stamps[i][j] = 1;
+                    stamps[x][y] = 1;
             }
         }
 
